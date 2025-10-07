fc95d642b21ab1644e90a50c9583807787076cf1
harshG775 <hgaur491@gmail.com>
2025-10-07 18:41:14 +0530

    animated cards

--- src/routes/page.jsx
+++ src/routes/page.jsx
@@ -1,10 +1,49 @@
+import { useState } from "react";
+
 export default function RootPage() {
     return (
-        <div className=" flex flex-col items-center justify-center min-h-screen bg-background text-center p-6">
-            <h1 className="text-4xl font-bold text-secondary-foreground mb-6">RootRoute</h1>
-            <div className="bg-background p-8 rounded-lg shadow-md mb-6">
-                <div className="flex gap-2">Welcome</div>
-            </div>
+        <div className="grid place-content-center min-h-screen">
+            <Cards />
         </div>
+    );
+}
+function Cards() {
+    const [isFull, setIsFull] = useState(true);
+    return (
+        <section className="relative size-52 group p-4">
+            <div
+                className={`${"size-52 bg-red-400 grid place-content-center text-4xl rounded-2xl absolute top-0 left-0 transition-transform duration-500 delay-200"}
+                     ${isFull ? "group-hover:-translate-y-[110%]" : "group-hover:-translate-y-10/12"}`}
+            >
+                <img src="/angry.jpeg" alt="angry" className="object-contain" />
+            </div>
+            <div
+                className={`${"size-52 bg-yellow-400 grid place-content-center text-4xl rounded-2xl absolute top-0 left-0 transition-transform duration-500 delay-200 origin-bottom-left "}
+                      ${
+                          isFull
+                              ? "group-hover:-translate-x-[110%] group-hover:-translate-y-25"
+                              : "group-hover:-translate-x-12 group-hover:-rotate-45"
+                      }`}
+            >
+                <img src="/hat.jpg" alt="hat" className="object-contain" />
+            </div>
+            <div
+                className={`${"size-52 bg-blue-400 grid place-content-center text-4xl rounded-2xl absolute top-0 left-0 transition-transform duration-500 delay-200 origin-bottom-right "}
+                     ${
+                         isFull
+                             ? "group-hover:translate-x-[110%] group-hover:-translate-y-25"
+                             : "group-hover:translate-x-12 group-hover:rotate-45"
+                     }`}
+            >
+                <img src="/face.jpeg" alt="face" className="object-contain" />
+            </div>
+            <div
+                onClick={() => setIsFull((e) => !e)}
+                onMouseLeave={() => setIsFull(false)}
+                className={`${"size-52 bg-orange-400 grid place-content-center text-4xl rounded-2xl absolute top-0 left-0 transition-transform duration-500 delay-200"}`}
+            >
+                <img src="/monkey.jpeg" alt="monkey" className="object-contain" />
+            </div>
+        </section>
     );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from "url";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import fs from "fs";
+// import fs from "fs";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -18,13 +18,13 @@ export default defineConfig({
     server: {
         port: 4000,
         host: true,
-        https: {
-            // NOTE: this is a self-signed certificate
-            // choco install mkcert
-            // mkcert -install
-            // Generate with: mkcert localhost
-            key: fs.readFileSync("./localhost-key.pem"),
-            cert: fs.readFileSync("./localhost.pem"),
-        },
+        // https: {
+        //     // NOTE: this is a self-signed certificate
+        //     // choco install mkcert
+        //     // mkcert -install
+        //     // Generate with: mkcert localhost
+        //     key: fs.readFileSync("./localhost-key.pem"),
+        //     cert: fs.readFileSync("./localhost.pem"),
+        // },
     },
 });
