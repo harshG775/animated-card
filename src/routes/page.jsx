bbe955ab99ea713f717d1d328a08bd5e0345e912
harshG775 <hgaur491@gmail.com>
2025-10-07 18:48:24 +0530

    making it responsive

--- src/routes/page.jsx
+++ src/routes/page.jsx
@@ -10,15 +10,15 @@ export default function RootPage() {
 function Cards() {
     const [isFull, setIsFull] = useState(true);
     return (
-        <section className="relative size-52 group p-4">
+        <section className="relative size-32 md:size-52 group p-4">
             <div
-                className={`${"size-52 bg-red-300 grid place-content-center text-4xl rounded-3xl absolute top-0 left-0 transition-transform duration-500 delay-200"}
+                className={`${"size-32 md:size-52 bg-red-300 grid place-content-center text-4xl rounded-3xl absolute top-0 left-0 transition-transform duration-500 delay-200"}
                      ${isFull ? "group-hover:-translate-y-[110%]" : "group-hover:-translate-y-10/12"}`}
             >
                 <img src="/angry.png" alt="angry" className="object-contain" />
             </div>
             <div
-                className={`${"size-52 bg-yellow-300 grid place-content-center text-4xl rounded-3xl absolute top-0 left-0 transition-transform duration-500 delay-200 origin-bottom-left "}
+                className={`${"size-32 md:size-52 bg-yellow-300 grid place-content-center text-4xl rounded-3xl absolute top-0 left-0 transition-transform duration-500 delay-200 origin-bottom-left "}
                       ${
                           isFull
                               ? "group-hover:-translate-x-[110%] group-hover:-translate-y-25"
@@ -28,7 +28,7 @@ function Cards() {
                 <img src="/hat.png" alt="hat" className="object-contain" />
             </div>
             <div
-                className={`${"size-52 bg-blue-300 grid place-content-center text-4xl rounded-3xl absolute top-0 left-0 transition-transform duration-500 delay-200 origin-bottom-right "}
+                className={`${"size-32 md:size-52 bg-blue-300 grid place-content-center text-4xl rounded-3xl absolute top-0 left-0 transition-transform duration-500 delay-200 origin-bottom-right "}
                      ${
                          isFull
                              ? "group-hover:translate-x-[110%] group-hover:-translate-y-25"
@@ -40,7 +40,7 @@ function Cards() {
             <div
                 onClick={() => setIsFull((e) => !e)}
                 onMouseLeave={() => setIsFull(false)}
-                className={`${"size-52 bg-orange-300 grid place-content-center text-4xl rounded-3xl absolute top-0 left-0 transition-transform duration-500 delay-200"}`}
+                className={`${"size-32 md:size-52 bg-orange-300 grid place-content-center text-4xl rounded-3xl absolute top-0 left-0 transition-transform duration-500 delay-200"}`}
             >
                 <img src="/monkey.png" alt="monkey" className="object-contain" />
             </div>
